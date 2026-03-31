395b54fde504bd109fdb19b9b491be83e014fdc8
Divyansh Jha <divyanshjha30@gmail.com>
2026-03-31 18:53:02 +0530

    Fix: add root-level re-exports for OpenEnv validator compatibility

---
+++ __init__.py
@@ -0,0 +1,5 @@
+"""OpenPM: Project Management RL Environment for OpenEnv Hackathon"""
+
+from openpm_env import PMAction, PMObservation, PMState, PMEnv
+
+__all__ = ["PMAction", "PMObservation", "PMState", "PMEnv"]
---
+++ client.py
@@ -0,0 +1,5 @@
+"""OpenPM Environment Client - Re-exports from openpm_env"""
+
+from openpm_env.client import PMEnv
+
+__all__ = ["PMEnv"]
